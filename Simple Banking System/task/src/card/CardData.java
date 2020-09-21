8b8d5623220691c099a113a2b1b9d0f3a24ddb0f
Sergey Rakov <rakovsergey94@gmail.com>
2020-09-21 20:12:01 +0300

    add CreateCard

--- Simple Banking System/task/src/card/CardData.java
+++ Simple Banking System/task/src/card/CardData.java
@@ -3,5 +3,25 @@ package card;
 import java.math.BigInteger;
 
 public class CardData {
-    private BigInteger
+    private final BigInteger cardNumber;
+    private final short password;
+    private long balance;
+
+    public CardData(BigInteger cardNumber, short password) {
+        this.cardNumber = cardNumber;
+        this.password = password;
+        this.balance = 0;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "CardData{" +
+                "cardNumber=" + cardNumber +
+                ", password=" + password +
+                '}';
+    }
 }
